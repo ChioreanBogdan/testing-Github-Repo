5f24b25f25211b85614ad6a480dad664bc28dd37
ChioreanBogdan <41981243+ChioreanBogdan@users.noreply.github.com>
2024-03-18 16:00:21 +0200

    added callback

--- src/app/api/auth/[...nextauth].js/route.ts
+++ src/app/api/auth/[...nextauth].js/route.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "@/lib/auth";
+//import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/db/prisma";
 import { env } from "@/lib/env";
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -9,6 +9,22 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 //import { authOptions } from "@/app/lib/auth";
 
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
+        GoogleProvider({
+            clientId: env.GOOGLE_CLIENT_ID,
+            clientSecret: env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    callbacks: {
+        session({ session, user }) {
+            session.user.id = user.id;
+            return session;
+        },
+    },
+}
+
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST };
